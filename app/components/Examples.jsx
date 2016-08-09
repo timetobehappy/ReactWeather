6cb2b0ec158fbb3443037a6f9df93822de2a6054
timetobehappy <jibiu@outlook.com>
2016-08-08 20:28:36 -0500

    Heroku support and div in examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,8 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
+    <div>
     <h2>Examples Component!</h2>
     <p>Welcome to examples page!</p>
+    </div>
   );
 };
 module.exports = Examples;
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ var express = require('express');
 
 //Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+//middleware to handle https to http for openweathermap api
+
+app.use(function (req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  } else {
+    debugger;
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
 
-app.listen(3000,function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is up on port' + PORT);
 });
